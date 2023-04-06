e39df6877d5bc7192dbeee12d41de2dcc26b61b5
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2023-04-06 12:14:16 +1000

    Removed Commented Out Code

--- src/components/CurrentChat.jsx
+++ src/components/CurrentChat.jsx
@@ -6,32 +6,6 @@ function CurrentChat({ message, username }) {
 
     return (
         <div className="text-sm md:text-base px-2 py-1 ml-4 md:ml-12">
-            {/* <table>
-                <tbody>
-                    {
-                        message.profile.username === username
-                        ?
-                        <tr className=''>
-                            <td className='text-right w-32 md:w-52 lg:w-60 pl-2 md:pl-8 pr-8 font-bold align-text-top break-all'>
-                                {message.profile.username}:
-                            </td>
-                            <td>
-                                {content}
-                            </td>
-                        </tr>
-                        :
-                        <tr className=''>
-                            <td className='text-right w-32 md:w-52 lg:w-60 pl-2 md:pl-8 pr-8 align-text-top break-all'>
-                                {message.profile.username}:
-                            </td>
-                            <td>
-                                {content}
-                            </td>
-                        </tr>
-                    }
-                </tbody>
-            </table> */}
-
             {
                 message.profile.username === username
                 ?
